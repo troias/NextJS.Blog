ea2a04ce38ac9273c49bfee559c6dc30310dd06a
troy <hunter007nz@gmail.com>
2021-10-25 02:16:16 +1300

    trouble shooting build

--- next.config.js
+++ next.config.js
@@ -12,8 +12,8 @@ module.exports = (phase) => {
         MONGO_DB_HOST: 'mongodb+srv://',
         MONGO_DB_USERNAME: 'troias',
         MONGO_DB_PASS: 'troy20flavell',
-        MONGO_DB_NAME:  'mysiteproduction',
-        MONGO_DB_HOST_CLUSTER: `@cluster0.fzmw2.mongodb.net/mysiteproduction?retryWrites=true&w=majority`,
+        MONGO_DB_NAME:  'comment',
+        MONGO_DB_HOST_CLUSTER: `@cluster0.fzmw2.mongodb.net/comment?retryWrites=true&w=majority`,
       }
 
     }
@@ -26,8 +26,8 @@ module.exports = (phase) => {
         MONGO_DB_HOST: 'mongodb+srv://',
         MONGO_DB_USERNAME: 'troias',
         MONGO_DB_PASS: 'troy20flavell',
-        MONGO_DB_NAME:  'mysiteproduction',
-        MONGO_DB_HOST_CLUSTER: `@cluster0.fzmw2.mongodb.net/mysiteproduction?retryWrites=true&w=majority`,
+        MONGO_DB_NAME:  'comment',
+        MONGO_DB_HOST_CLUSTER: `@cluster0.fzmw2.mongodb.net/comment?retryWrites=true&w=majority`,
       },
       experimental: { esmExternals: true },
       webpack: (config, { isServer }) => {
